24d4d95731d4e85a990aabcae0ce48904efbe05f
SUNRIT SARKAR <81092346+Sunrit07@users.noreply.github.com>
2025-07-18 01:20:58 +0530

    First dotnet file uploaded

---
+++ Assignment1/Assignment1/Class2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class Class2
+    {
+        static void Main()
+        {
+            int num1, num2;
+            Console.WriteLine("enter num1");
+            num1 = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine("enter num2");
+            num2 = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine($"Sum of {num1} and {num2} is {num1 + num2}");
+            Console.WriteLine($"Difference of {num1} and {num2} is {num1 - num2}");
+            Console.WriteLine($"Product of {num1} and {num2} is {num1 * num2}");
+            Console.WriteLine($"Quotient of {num1} and {num2} is {num1 / num2}");
+
+        }
+    }
+}
---
+++ Assignment1/Assignment1/Class3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class Class3
+    {
+        static void Main()
+        {
+            int num1, num2;
+            int ch;
+            Console.WriteLine("enter num1");
+            num1 = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine("enter num2");
+            num2 = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine("enter choice");
+            ch = Convert.ToByte(Console.ReadLine());
+            if (ch == 1)
+                Console.WriteLine($"Sum of {num1} and {num2} is {num1 + num2}");
+            else if (ch == 2)
+                Console.WriteLine($"Difference of {num1} and {num2} is {num1 - num2}");
+            else if (ch == 3)
+                Console.WriteLine($"Product of {num1} and {num2} is {num1 * num2}");
+            else if (ch == 4)
+                Console.WriteLine($"Quotient of {num1} and {num2} is {num1 / num2}");
+            else
+                Console.WriteLine("invalid choice");
+        }
+    }
+}
