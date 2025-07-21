e1fe5d32ac79af1bf59136efad1dd1c6c5c7593b
SUNRIT SARKAR <81092346+Sunrit07@users.noreply.github.com>
2025-07-22 00:05:03 +0530

    Day-4-OOPs commited

---
+++ Day4_OOPSEg/Day4_OOPSEg/Shape.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4_OOPSEg
+{
+    internal class Shape
+    {
+        public string Color { get; set; }
+        public float Area { get; set; }
+        public virtual void Accept()
+        {
+
+            Console.WriteLine("Enter Color: ");
+            Color = Console.ReadLine();
+        }
+
+        public virtual void CalculateArea()
+        {
+            Console.WriteLine("Calculate area method of shape class: ");
+        }
+        public virtual void Display()
+        {
+            Console.WriteLine("Color: " + Color);
+            Console.WriteLine("Area: " + Area);
+        }
+    }
+
+    class Circle : Shape
+    {
+        public int Radius { get; set; }
+        public override void  Accept()
+        {
+            base.Accept();
+            Console.WriteLine("Enter radius: ");
+            Radius = Convert.ToInt32(Console.ReadLine());
+
+        }
+
+        public override void CalculateArea()
+        {
+            base .CalculateArea();
+            Area = float.Parse((3.14 * Radius * Radius).ToString()); ;
+
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("Radius: " + Radius);
+            base.Display();
+        }
+    }
+}
---
+++ Day4_OOPSEg/Day4_OOPSEg/ShapeClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4_OOPSEg
+{
+    internal class ShapeClient
+    {
+        public static void Main()
+        {
+            Shape s = new Circle();
+            s.Accept();
+            s.CalculateArea();
+            s.Display();
+        }
+    }
+}
