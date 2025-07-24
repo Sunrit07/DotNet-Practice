085b9c7b880e98d5b930ca71794f5e35a9d0d34d
SUNRIT SARKAR <81092346+Sunrit07@users.noreply.github.com>
2025-07-24 23:02:29 +0530

    File commited

---
+++ Day5_File/Day5_File/Emp.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Day5_File
+{
+    public class Emp
+    {
+        public static void Main()
+        {
+            Emp emp = new Emp("Sunrit", 24);
+            Emp.Serialize(emp, "abc.txt");
+            Emp.Deserialize("abc.txt");
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Emp() { }
+        public Emp(string name, int age) { Name = name; Age = age; }
+        public static void Serialize(Emp emp, string filename)
+        {
+            XmlSerializer mySerializer = new XmlSerializer(typeof(Emp));
+            StreamWriter stream = new StreamWriter(filename);
+            Console.WriteLine("Writing Employee Information");
+            try
+            {
+                mySerializer.Serialize(stream, emp);
+            }
+            catch (SerializationException ex)
+            {
+                Console.WriteLine("Exception for Serialization data: " + ex.Message);
+                throw;
+            }
+            finally
+            {
+                stream.Close();
+                Console.WriteLine("Successfully wrote Employye Information ");
+            }
+        }
+
+        public static void Deserialize(string filename)
+        {
+            Emp mp = null;
+            XmlSerializer mySerializer = new XmlSerializer(typeof(Emp));
+            FileStream myFileStream = new FileStream(filename, FileMode.Open);
+            Console.WriteLine("Reading Employee Information");
+            try
+            {
+                mp = (Emp)mySerializer.Deserialize(myFileStream);
+
+            }
+            catch (SerializationException ex)
+            {
+                Console.WriteLine("Exception for Deserialize data.." + ex.Message);
+                throw;
+            }
+            finally
+            {
+                myFileStream.Close();
+            }
+            if (mp != null)
+            {
+                Console.WriteLine("Employee Name: {0}", mp.Name);
+                Console.WriteLine("Employee Id: {0}", mp.Age.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Deserialize employee null Value;");
+            }
+        }
+    }
+}
---
+++ Day5_File/Day5_File/FileEg.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5_File
+{
+    internal class FileEg
+    {
+        public static void Main()
+        {
+            WriteToFile();
+            ReadFromFile();
+
+        }
+        private static void ReadFromFile()
+        {
+            FileStream fs = new FileStream("WiproFile.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            string? line; //nullable variable
+            while ((line = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        private static void WriteToFile()
+        {
+            FileStream fs = new FileStream("WiproFile.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            Console.WriteLine("Enter the text to be written in the file: ");
+            string username = Console.ReadLine();
+            sw.WriteLine("Welcome " + username + DateTime.Now);
+            sw.Flush();
+            fs.Close();
+        }
+    }
+}
