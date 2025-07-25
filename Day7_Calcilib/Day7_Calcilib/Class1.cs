8c48a7dce454acdbd1ee5cfad0426cac017fae74
SUNRIT SARKAR <81092346+Sunrit07@users.noreply.github.com>
2025-07-25 23:16:48 +0530

    CalculatorLIb commited

---
+++ Day7_Calcilib/Day7_Calcilib/Class1.cs
@@ -0,0 +1,16 @@
+﻿namespace Day7_Calcilib
+{
+    // For reusability
+    public interface ICalculator
+    {
+        int add(int x, int y);
+    }
+
+    public class Calculator : ICalculator
+    {
+        public int add(int x, int y)
+        {
+            return x+y;
+        }
+    }
+}
