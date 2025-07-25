0dffa3478044c42055f107ec6d6f3d00989bedea
SUNRIT SARKAR <81092346+Sunrit07@users.noreply.github.com>
2025-07-25 23:20:57 +0530

    CalciProject commited

---
+++ Day7_CalciProject/Day7_CalciProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using Day7_Calcilib;      // it should be public all interface and classes too
+namespace CalciProject
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Enter 2 numbers: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            ICalculator obj = new Calculator();
+            Console.WriteLine(obj.add(a, b));
+        }
+    }
+}
